e7f03eb0eb973e8ae821486bc920f0e1bb852cc4
IfeAdemola <ifeoluwaademola17@gmail.com>
2024-08-29 16:20:24 +0200

    New informed band combinations

--- code/datasets.py
+++ code/datasets.py
@@ -4,16 +4,20 @@ import matplotlib.pyplot as plt
 import pickle
 import os
 import torch
+from utils import *
 from typing import List, Dict, Any
 from tqdm import tqdm
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 from typing import List, Dict, Any
 
-S2_BANDS_LR = [0,1,2,6]
-# S2_BANDS_LR = [6]
-S2_BANDS_MR = [3,4,5,7,8,9]
-SEASON_BAND = [10]
+
+RGB = [0, 1, 2]
+NIR = [6]
+RED_EDGE = [3, 4, 5, 7]
+SWIR = [7, 8, 9]
+SEASON = [10]
+
 def compute_num_classes(root_dir, label):
         """Compute the number of unique classes in the dataset masks"""
         all_masks = []
@@ -29,38 +33,39 @@ def compute_num_classes(root_dir, label):
         num_classes = len(set(all_masks))
         return num_classes
 
-def load_bands(use_rgb, use_lr, use_mr, use_season):
+def load_bands(use_rgb, use_red, use_nir, use_red_edge, use_swir, use_season):
     bands_selected = []
+    
     if use_rgb:
-        bands_selected = bands_selected + S2_BANDS_LR[:-1]
-    if use_lr:
-        bands_selected = list(set(bands_selected + S2_BANDS_LR))
-    if use_mr:
-        bands_selected = bands_selected + S2_BANDS_MR
-    if use_season:
-        bands_selected = bands_selected + SEASON_BAND
-    return bands_selected
+        bands_selected += RGB        
+    if use_red:
+        bands_selected += [RGB[0]]
+    if use_nir:
+        bands_selected += NIR        
+    if use_red_edge:
+        bands_selected += RED_EDGE
+    if use_swir:
+        bands_selected += SWIR
 
-def get_ninputs(use_rgb, use_lr, use_mr, use_season):
-    n_inputs = 0
-    if use_rgb:
-        n_inputs += len(S2_BANDS_LR[:-1])
-    if use_lr and not use_rgb:
-        n_inputs += len(S2_BANDS_LR)
-    if use_lr and use_rgb:
-        n_inputs += 1
-    if use_mr:
-        n_inputs += len(S2_BANDS_MR)
+    # If none are selected, default to RGB only
+    if not (use_rgb or use_nir or use_red_edge or use_swir or use_season):
+        bands_selected = RGB
+
+    # Always include SEASON if selected
     if use_season:
-        n_inputs += len(SEASON_BAND)
+        bands_selected += SEASON
+        
+    return list(set(bands_selected)) 
+
+def get_ninputs(use_rgb, use_red, use_nir, use_red_edge, use_swir, use_season):
+    n_inputs = len(load_bands(use_rgb, use_red, use_nir, use_red_edge, use_swir, use_season))
     return n_inputs
-
 
 class Forest(Dataset):
     """PyTorch dataset class for loading numpy arrays from pickle files"""
 
     def __init__(self, root_dir, label_structure=False, transform=None, 
-                 use_rgb=False, use_lr=False, use_mr=False, use_season=False):
+                 use_rgb=False, use_red=False, use_nir=False, use_red_edge=False, use_swir=False, use_season=False):
         """
         Args:
             root_dir (string): Directory with pickle files.
@@ -72,24 +77,27 @@ class Forest(Dataset):
         super(Forest, self).__init__
 
         # make sure parameters are okay
-        if not (use_rgb or use_lr or use_mr or use_season):
-            raise ValueError("No input specified, set at least one of "
-                             + "use_[rgb, lr, mr, season] to True!")
+        if not (use_rgb or use_red or use_nir or use_red_edge or use_swir or use_season):
+            print("No input specified; defaulting to RGB usage.")
+            # raise ValueError("No input specified, set at least one of "
+            #                  + "use_[rgb, lr, mr, season] to True!")
 
         # make sure parent dir exists
         assert os.path.exists(root_dir)
         self.root_dir = root_dir
 
         self.use_rgb = use_rgb
-        self.use_lr = use_lr
-        self.use_mr = use_mr
+        self.use_red = use_red
+        self.use_nir = use_nir
+        self.use_red_edge = use_red_edge
+        self.use_swir = use_swir
         self.use_season = use_season
 
         self.label_structure = label_structure
 
-        self.selected_bands = load_bands(self.use_rgb, self.use_lr, self.use_mr, self.use_season)
+        self.selected_bands = load_bands(self.use_rgb, self.use_red, self.use_nir, self.use_red_edge, self.use_swir, self.use_season)
 
-        self.n_inputs = get_ninputs(use_rgb, use_lr, use_mr, use_season)
+        self.n_inputs = get_ninputs(use_rgb, use_red, use_nir, use_red_edge, use_swir, use_season)
         if label_structure:
             self.num_classes = 6
             self.label_structure = True
@@ -113,34 +121,28 @@ class Forest(Dataset):
         with open(file_name, 'rb') as f:
             sample = pickle.load(f)
 
-        # Extract image and mask from the numpy array
-        image = sample[:,:, load_bands(self.use_rgb, self.use_lr, self.use_mr, self.use_season)]
+        # Extract bands from the numpy array 
+        sensor_bands = load_bands(self.use_rgb, self.use_red, self.use_nir, self.use_red_edge, self.use_swir, self.use_season)
+
+        sensor_image = sample[:, :, sensor_bands]
+
 
         if self.label_structure == False:
             mask = sample[:, :, 11]  # region type [0,1,2]
         elif self.label_structure == True:
             mask = sample[:,:,12]  # more specific region type [0,1,...,5]
 
-        #Normalise the image
-        """this would not work for season band- rectify"""
-        min_val = np.min(image)
-        max_val = np.max(image)
-        image_nor = (image - min_val) / (max_val - min_val)  
 
-        # Convert numpy arrays to PyTorch tensors
-        image_nor = torch.from_numpy(image_nor).float()
+        normalised_image = torch.from_numpy(sensor_image).float() # change variable name (it's not a normalisation)
         mask = torch.from_numpy(mask).long()
-
-        # Normalize the image
-        # image = self.normalize(image)
 
         # Apply transform if specified
         if self.transform:
-            image_nor, mask = self.transform((image_nor, mask))
+            normalised_image, mask = self.transform((normalised_image, mask))
 
         # return {'image':image_nor, 'label':mask}
     
-        return image_nor, mask
+        return normalised_image, mask
     
     
 
